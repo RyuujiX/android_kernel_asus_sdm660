9d4c2577c0124635f1f72daf99132338b90672d6
Sultanxda <sultanxda@gmail.com>
2017-04-30 12:33:05 -0700

    scsi: sd: Configure the I/O sched switcher for sda too
    We're actively using both sda (/data) and sde (/system) at runtime, so
    have both block devices use noop when the screen is off.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>
    Signed-off-by: ahmedradaideh <ahmed.radaideh@gmail.com>
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>

--- drivers/scsi/sd.c
+++ drivers/scsi/sd.c
@@ -52,6 +52,7 @@
 #include <linux/slab.h>
 #include <linux/pm_runtime.h>
 #include <linux/pr.h>
+#include <linux/iosched_switcher.h>
 #include <asm/uaccess.h>
 #include <asm/unaligned.h>
 
@@ -3160,6 +3161,10 @@ static int sd_probe(struct device *dev)
 
 	get_device(&sdkp->dev);	/* prevent release before async_schedule */
 	async_schedule_domain(sd_probe_async, sdkp, &scsi_sd_probe_domain);
+
+	if (!strcmp(sdkp->disk->disk_name, "sda") ||
+		!strcmp(sdkp->disk->disk_name, "sde"))
+		init_iosched_switcher(sdp->request_queue);
 
 	return 0;
 
