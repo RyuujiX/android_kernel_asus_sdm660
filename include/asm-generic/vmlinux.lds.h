9c4019e0ea155f4f08a5425beb4872ee744b5dc0
Divyanshu-Modi <divyan.m05@gmail.com>
2021-01-30 17:21:10 +0530

    Revert "vmlinux.lds.h: Add PGO and AutoFDO input sections"
    This reverts commit b60abd189e520ac975d8921d052c2dea130c89b2.
    
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>

--- include/asm-generic/vmlinux.lds.h
+++ include/asm-generic/vmlinux.lds.h
@@ -430,10 +430,7 @@
  * during second ld run in second ld pass when generating System.map */
 #define TEXT_TEXT							\
 		ALIGN_FUNCTION();					\
-		*(.text.hot .text.hot.*)				\
-		*(.text .text.fixup)					\
-		*(.text.unlikely .text.unlikely.*)			\
-		*(.text.unknown .text.unknown.*)			\
+		*(.text.hot .text .text.fixup .text.unlikely)		\
 		*(.ref.text)						\
 	MEM_KEEP(init.text)						\
 	MEM_KEEP(exit.text)						\
