194cb566bce85556b30770a09cadeb941c3a5f54
RyuujiX <saputradenny712@gmail.com>
2021-04-18 06:16:08 +0700

    Revert "power: hack fg_get_battery_temp to report reading from pa_therm0 as battery temperature"
    This reverts commit da3a06e8fdfa816bd687aee4f547568f5c9fa529.
    
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>

--- drivers/power/supply/qcom/fg-core.h
+++ drivers/power/supply/qcom/fg-core.h
@@ -463,7 +463,6 @@ struct fg_chip {
 	struct work_struct	esr_filter_work;
 	struct alarm		esr_filter_alarm;
 	ktime_t			last_delta_temp_time;
-	struct qpnp_vadc_chip	*vadc_dev;
 };
 
 /* Debugfs data structures are below */
