84d31a63f12a48b04988a5e8c6fcb96984115804
RyuujiX <saputradenny712@gmail.com>
2021-04-26 22:14:30 +0700

    wireguard: Upstream to v1.0.20210424
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>

--- drivers/net/wireguard/peer.c
+++ drivers/net/wireguard/peer.c
@@ -52,6 +52,7 @@ struct wg_peer *wg_peer_create(struct wg_device *wg,
 	kref_init(&peer->refcount);
 	skb_queue_head_init(&peer->staged_packet_queue);
 	wg_noise_reset_last_sent_handshake(&peer->last_sent_handshake);
+	set_bit(NAPI_STATE_NO_BUSY_POLL, &peer->napi.state);
 	netif_napi_add(wg->dev, &peer->napi, wg_packet_rx_poll,
 		       NAPI_POLL_WEIGHT);
 	napi_enable(&peer->napi);
