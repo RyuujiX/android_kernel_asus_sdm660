cf3d9e4bc7ddadae6fc9e868520d9353607fcf37
RyuujiX <saputradenny712@gmail.com>
2021-05-27 11:32:44 +0700

    [SQUASH] Revert Some Unnecessary Commits.
    This reverts commits :
    - kernel: printk: Allow logging to be toggled via sysfs
    - mm: allow __GFP_MOVABLE to use CMA memory
    - mdss: msm: dp: Silence log spam
    - drivers: char: adsprpc: Specify sync probe for fastrpc
    - ion/msm: move msm_ion_init to async probe
    - soc/qcom: move icnss_initialize to async probe
    - msm: kgsl: move kgsl_3d_init to async
    - proc: cmdline: Don't patch flags in recovery mode
    - proc: cmdline: Patch vbmeta device state flag for SafetyNet
    - proc: cmdline: Patch SafetyNet flags
    - ANDROID: xt_qtaguid: fix UAF race
    - thermal: lmh_interface: Silence log debugfs fail when debugfs is disabled
    - sched/fair: Compile out NUMA code entirely when NUMA is disabled
    - ARM64: dts: prefer qcom,kryo over arm,armv8
    - DRIVERS: SLIMBUS: Fix O.C
    - enable Slimbus O.C
    - Slimbus Overclock
    
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>

--- drivers/slimbus/slim-msm-ctrl.c
+++ drivers/slimbus/slim-msm-ctrl.c
@@ -92,8 +92,8 @@ enum mgr_intr {
 
 enum frm_cfg {
 	FRM_ACTIVE	= 1,
-	CLK_GEAR	= 11,
-	ROOT_FREQ	= 31,
+	CLK_GEAR	= 7,
+	ROOT_FREQ	= 11,
 	REF_CLK_GEAR	= 15,
 	INTR_WAKE	= 19,
 };
@@ -207,7 +207,7 @@ static irqreturn_t msm_slim_interrupt(int irq, void *d)
 		 * signalling completion/exiting ISR
 		 */
 		mb();
-		msm_slim_manage_tx_msgq(dev, false, NULL, 0);
+		msm_slim_manage_tx_msgq(dev, false, NULL);
 	}
 	if (stat & MGR_INT_RX_MSG_RCVD) {
 		u32 rx_buf[10];
