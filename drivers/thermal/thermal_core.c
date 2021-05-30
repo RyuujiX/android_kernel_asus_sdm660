71a84c5446f0cd78e58df5067f6fe102e4871eca
Alex Naidis <alex.naidis@linux.com>
2021-05-30 07:13:52 +0700

    thermal-core: Fix invalid sensor data being accepted
    In some cases the sensor is supplying invalid data by
    reporting the temperature as 0.
    
    In this case we need to fallback to the DEFAULT_TEMP
    which contains a safe value. This is better than just
    relying on the last value supplied by the sensor since
    it may be outdated at this time
    
    This fixes the invalid behaviour of temperature
    assisted scheduling based on temperature
    sensor sampling in msm-core which led to this
    issue being noticed.
    
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -448,7 +448,12 @@ int sensor_get_temp(uint32_t sensor_id, int *temp)
 		return -ENODEV;
 
 	ret = sensor->tz->ops->get_temp(sensor->tz, temp);
-
+	
+	if (!temp && !ret) {
+		pr_debug("thermal_core: Reporting default temperature.");
+		*temp = DEFAULT_TEMP;
+	}
+	
 	return ret;
 }
 EXPORT_SYMBOL(sensor_get_temp);
