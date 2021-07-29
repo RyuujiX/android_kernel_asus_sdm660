6788a19cceb1cfbff49e223a66f427b7461be776
RyuujiX <saputradenny712@gmail.com>
2021-07-29 07:06:05 +0700

    [SQUASH] Remove Performance Critical
    This reverts commits :
    - defconfig: Tune LITTLE and big cpumask
    - cpumask: Add cpumasks for big and LITTLE CPU clusters
    - msm: mdss: Mark display-wake kthread as performance critical
    - msm: mdss: Power on display asynchronously as early as possible
    - msm: mdss: Mark IRQ and important kthreads as performance critical
    - kernel: Add API to mark IRQs and kthreads as performance critical
    
    Also Committed:
    - X00TD: drivers: input: Remove Perf Critical on cdfinger
    
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>

--- drivers/input/fingerprint/cdfinger_X00TD/cdfingerfp.c
+++ drivers/input/fingerprint/cdfinger_X00TD/cdfingerfp.c
@@ -315,7 +315,7 @@ static int cdfinger_init_irq(struct cdfingerfp_data *pdata)
 	if (isInit == 1)
 		return 0;
 
-	error = request_threaded_irq(gpio_to_irq(pdata->irq_num),cdfinger_eint_handler,NULL, IRQF_TRIGGER_RISING | IRQF_PERF_CRITICAL,"cdfinger_eint", (void*)pdata);
+	error = request_threaded_irq(gpio_to_irq(pdata->irq_num),cdfinger_eint_handler,NULL, IRQF_TRIGGER_RISING,"cdfinger_eint", (void*)pdata);
 	if (error < 0)
 	{
 		CDFINGER_ERR("irq init err\n");
