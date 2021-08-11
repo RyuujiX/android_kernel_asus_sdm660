efa7600899a7b52f951aaac25acc52ff47026674
RyuujiX <saputradenny712@gmail.com>
2021-08-12 04:50:45 +0700

    kernel: {blu_}schedutil: Tune Up Rate Limit
    * Balanced
    
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>

--- kernel/sched/cpufreq_blu_schedutil.c
+++ kernel/sched/cpufreq_blu_schedutil.c
@@ -31,7 +31,7 @@ unsigned long boosted_cpu_util(int cpu);
 #define cpufreq_disable_fast_switch(x)
 #define LATENCY_MULTIPLIER	    (2000)
 #define SUGOV_KTHREAD_PRIORITY	50
-#define UP_RATE_LIMIT_US 90000
+#define UP_RATE_LIMIT_US 500
 #define DOWN_RATE_LIMIT_US 500
 
 struct sugov_tunables {
--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -26,7 +26,7 @@ unsigned long boosted_cpu_util(int cpu);
 #define cpufreq_driver_fast_switch(x, y) 0
 #define cpufreq_enable_fast_switch(x)
 #define cpufreq_disable_fast_switch(x)
-#define UP_RATE_LIMIT_US			(90000)
+#define UP_RATE_LIMIT_US			(500)
 #define DOWN_RATE_LIMIT_US			(500)
 #define SUGOV_KTHREAD_PRIORITY	50
 
